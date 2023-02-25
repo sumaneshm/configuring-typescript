47d192fcfb7720822d6ee6a697a0eb50557b0843
Sumanesh Magarabooshanam <sumaneshm@users.noreply.github.com>
2023-02-25 17:48:52 +0800

    added type definition files

--- src/app/Main.ts
+++ src/app/Main.ts
@@ -8,16 +8,16 @@ export class Main {
 
         return `
             <div class="mt-4">
-              <h2>Globoticket Portal</h2>
+              <h2>${emphasizeString("Globoticket Portal", 5)}</h2>
               <table class="table-bordered">
               <thead>
                 <th class="p-2">Concert Name</th>
                 <th class="p-2">Price</th>
-                <th class="p-2">Remaining Tickets</th>
+                <th class="p-2">${emphasizeString("Remaining Tickets", 10)}</th>
               </thead>
               <tbody>
 
-                    ${_.shuffle(this.ticketInfo).map(ticket => `<tr>
+                    ${_.uniq(_.shuffle(this.ticketInfo)).map(ticket => `<tr>
                         <td class="p-2">${emphasizeString(ticket.concertName, 3)}</td>
                         <td class="p-2">${ticket.price}</td>
                         <td class="p-2">${ticket.quantity}</td>
